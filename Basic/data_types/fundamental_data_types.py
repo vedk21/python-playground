70c8b94a48b258090d47f4ac4c1e82e34ec96e92
vedk21 <vedantkakade.cp@gmail.com>
2020-05-18 23:59:09 +0530

    Added: data-types - fundamental data types

---
+++ Basic/data_types/fundamental_data_types.py
@@ -0,0 +1,10 @@
+# Basic Data Types
+int # integer numeric type
+float # floating point number
+bool # boolean
+str # string (set of characters)
+list # collection of heterogeneous types (mutable)
+tuple # collection of heterogeneous types (immutable)
+set # collection of unique heterogeneous types (immutable)
+dict # key value pair map
+None # no-value type
