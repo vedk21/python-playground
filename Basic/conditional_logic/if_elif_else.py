469aa772880958abc9921dbb9392de59942e3ce7
vedk21 <vedantkakade.cp@gmail.com>
2020-05-22 00:15:35 +0530

    Added: conditional-logic - if elif else ladder, ternary operation

---
+++ Basic/conditional_logic/if_elif_else.py
@@ -0,0 +1,10 @@
+# if elif else ladder
+
+age = 24
+
+if age > 21:
+  print('old enough to open an account and have bonus rewards')
+elif age > 18:
+  print('old enough to open an account')
+else:
+  print('Sorry, you are not allowed')
---
+++ Basic/conditional_logic/ternery_operation.py
@@ -0,0 +1,5 @@
+# Ternary operation
+
+age = 24
+
+print('old enough to drive' if age > 18 else 'not allowed to drive') # 'old enough to drive'
