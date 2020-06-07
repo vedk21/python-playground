14236742c626070c1b4d347694c6a19c2db70331
vedk21 <vedantkakade.cp@gmail.com>
2020-06-07 16:24:52 +0530

    Added: Modules - built-in-modules - array.py

---
+++ Advanced/Modules/BuiltInModules/array_test.py
@@ -0,0 +1,8 @@
+# Array package
+
+from array import array
+
+arr = array('i', [1, 2, 3, 4, 5])
+print(arr[3]) # 4
+
+# In python, lists are dynamic type data-structure, but when we need fixed size linear array data structure we use array package.
