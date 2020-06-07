f5ab5901f1603befad29774f06f5db7f4c01e8ad
vedk21 <vedantkakade.cp@gmail.com>
2020-06-07 16:03:26 +0530

    Added: Modules - virtual environment

---
+++ Advanced/Modules/virtual_env.py
@@ -0,0 +1,7 @@
+# In python, we need to have virtual environments so that we can maintain different versions of packages for different projects
+
+# example:
+# project 1 is using numpy==4.1.1
+# and we need numpy==3.5.6 in project 2
+# then we add pipenv or virtualenv in our projects
+# by using these virtual environments we can maintain different sets of packages versions according to project requirement, instead of globally installing same package version for all.
