40823eab7d8d8e67f00955c3c74a00a6562e9ed9
vedk21 <vedantkakade.cp@gmail.com>
2020-06-26 22:52:53 +0530

    Added: Web Development - contact form submission, request handling at backend

--- Professional/WebDevelopment/Project/server.py
+++ Professional/WebDevelopment/Project/server.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('./index.html')
+  return render_template('./index.html')
 
 # generic html routing
 @app.route('/<string:html_page>')
 def generic_page(html_page='index.html'):
-    return render_template(html_page)
+  return render_template(html_page)
+
+# handle contact form request
+@app.route('/submit_contact_form', methods = ['POST', 'GET'])
+def submit_contact_form():
+  if request.method == 'POST':
+    form_data = request.form.to_dict()
+    print(form_data)
+    return 'Form submitted'
+  else:
+    return 'Error: Please try again later.'
